6194e43c881e07652450895c4e031aa635c195b4
mavt19 <mavt10rm@gmail.com>
2020-09-05 11:36:53 -0500

    Updates

---
+++ precache-manifest.6c591b93ba6f6caac52e9cfe4bee8977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ccac9c812a4d50da71b92950d61d43",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "7956a53a88f1b60a5f27",
+    "url": "/react-shopping-cart/static/css/main.e5481950.chunk.css"
+  },
+  {
+    "revision": "9840e9567f5d5b86c354",
+    "url": "/react-shopping-cart/static/js/2.08d7c96c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-shopping-cart/static/js/2.08d7c96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7956a53a88f1b60a5f27",
+    "url": "/react-shopping-cart/static/js/main.333722f9.chunk.js"
+  },
+  {
+    "revision": "341faa49dfc013baa22a",
+    "url": "/react-shopping-cart/static/js/runtime-main.02ce5720.js"
+  }
+]);
